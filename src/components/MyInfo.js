d63ef35ba7e85504b4e6cd4205248acadadebee8
Cindy Chen <cchen4@cnm.edu>
2021-04-27 10:49:50 -0600

    playing around w/ inline styling

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -4,11 +4,19 @@ export function MyInfo() {
     const firstName = "Cindy";
     const lastName = "Chen";
 
+    const styles = {
+        color: "indigo",
+        backgroundColor: "pink",
+        fontSize: 30
+    }
+
     return (
         <>
             <h1>Hello, my name is {firstName + ' ' + lastName}</h1>
-            <h1>Hello, {`${firstName} ${lastName}`} This does the same thing</h1>
-            <p>This is a paragraph about me...</p>
+            <h1 style={{color: "darkgreen", backgroundColor: "lavender"}}>Hello, {`${firstName} ${lastName}`} This does the same thing</h1>
+
+
+            <p style={styles}>This is a paragraph about me...</p>
             <ul>
                 <li>
                     New Zealand
