cf8d27e5c6a7ef96bbc398ae6431a6d210e4b3d1
Cindy Chen <cchen4@cnm.edu>
2021-04-27 10:40:12 -0600

    JSX to JS and back

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export function MyInfo() {
+    const firstName = "Cindy";
+    const lastName = "Chen";
+
     return (
         <>
-            <h1>Cindy Chen</h1>
+            <h1>Hello, my name is {firstName + ' ' + lastName}</h1>
+            <h1>Hello, {`${firstName} ${lastName}`} This does the same thing</h1>
             <p>This is a paragraph about me...</p>
             <ul>
                 <li>
