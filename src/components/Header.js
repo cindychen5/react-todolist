ce32e1248734a2832a2f23084b9a17cbc935cd34
Cindy Chen <cchen4@cnm.edu>
2021-04-27 11:01:37 -0600

    playing around w/ date and JS inline styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,32 @@ import React from "react";
 
 
 export function Header() {
+    const date = new Date(2022, 4, 27, 12, 30, 15);
+    //year, month, date, hour, min, sec
+    const hours = date.getHours()
+    console.log(date)
+    // console.log(hours)
+    let timeOfDay
+
+    const styles = {
+        fontSize: 30
+    }
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+        styles.color = "green"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+        styles.color= "yellow"
+    } else {
+        timeOfDay = "night"
+        styles.color = "#D90000"
+    }
+
     return (
         <>
-            <header className="navbar">This is the header</header>
+            <header className="navbar" style={styles}>Good {timeOfDay}</header>
+
         </>
     )
 }
